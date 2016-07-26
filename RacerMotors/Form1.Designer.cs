a9a8a3599ff3f961b8d2df2b7e9f8e51cf502a70
al-metal <al-metal@bk.ru>
2016-07-26 14:40:08 +0400

    add btnPrice

--- RacerMotors/Form1.Designer.cs
+++ RacerMotors/Form1.Designer.cs
@@ -35,50 +35,57 @@
             this.tbKeywords = new System.Windows.Forms.TextBox();
             this.btnSaveTemplate = new System.Windows.Forms.Button();
             this.btnActualPrice = new System.Windows.Forms.Button();
+            this.btnPrice = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // rtbMiniText
             // 
-            this.rtbMiniText.Location = new System.Drawing.Point(12, 12);
+            this.rtbMiniText.Location = new System.Drawing.Point(9, 10);
+            this.rtbMiniText.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.rtbMiniText.Name = "rtbMiniText";
-            this.rtbMiniText.Size = new System.Drawing.Size(696, 154);
+            this.rtbMiniText.Size = new System.Drawing.Size(523, 126);
             this.rtbMiniText.TabIndex = 0;
             this.rtbMiniText.Text = "";
             // 
             // rtbFullText
             // 
-            this.rtbFullText.Location = new System.Drawing.Point(12, 172);
+            this.rtbFullText.Location = new System.Drawing.Point(9, 140);
+            this.rtbFullText.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.rtbFullText.Name = "rtbFullText";
-            this.rtbFullText.Size = new System.Drawing.Size(696, 154);
+            this.rtbFullText.Size = new System.Drawing.Size(523, 126);
             this.rtbFullText.TabIndex = 1;
             this.rtbFullText.Text = "";
             // 
             // tbTitle
             // 
-            this.tbTitle.Location = new System.Drawing.Point(12, 332);
+            this.tbTitle.Location = new System.Drawing.Point(9, 270);
+            this.tbTitle.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbTitle.Name = "tbTitle";
-            this.tbTitle.Size = new System.Drawing.Size(696, 22);
+            this.tbTitle.Size = new System.Drawing.Size(523, 20);
             this.tbTitle.TabIndex = 2;
             // 
             // tbDescription
             // 
-            this.tbDescription.Location = new System.Drawing.Point(12, 360);
+            this.tbDescription.Location = new System.Drawing.Point(9, 292);
+            this.tbDescription.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbDescription.Name = "tbDescription";
-            this.tbDescription.Size = new System.Drawing.Size(696, 22);
+            this.tbDescription.Size = new System.Drawing.Size(523, 20);
             this.tbDescription.TabIndex = 3;
             // 
             // tbKeywords
             // 
-            this.tbKeywords.Location = new System.Drawing.Point(12, 388);
+            this.tbKeywords.Location = new System.Drawing.Point(9, 315);
+            this.tbKeywords.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbKeywords.Name = "tbKeywords";
-            this.tbKeywords.Size = new System.Drawing.Size(696, 22);
+            this.tbKeywords.Size = new System.Drawing.Size(523, 20);
             this.tbKeywords.TabIndex = 4;
             // 
             // btnSaveTemplate
             // 
-            this.btnSaveTemplate.Location = new System.Drawing.Point(744, 360);
+            this.btnSaveTemplate.Location = new System.Drawing.Point(558, 292);
+            this.btnSaveTemplate.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnSaveTemplate.Name = "btnSaveTemplate";
-            this.btnSaveTemplate.Size = new System.Drawing.Size(163, 50);
+            this.btnSaveTemplate.Size = new System.Drawing.Size(122, 41);
             this.btnSaveTemplate.TabIndex = 5;
             this.btnSaveTemplate.Text = "Сохранить шаблон";
             this.btnSaveTemplate.UseVisualStyleBackColor = true;
@@ -86,19 +93,30 @@
             // 
             // btnActualPrice
             // 
-            this.btnActualPrice.Location = new System.Drawing.Point(724, 26);
+            this.btnActualPrice.Location = new System.Drawing.Point(543, 21);
+            this.btnActualPrice.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnActualPrice.Name = "btnActualPrice";
-            this.btnActualPrice.Size = new System.Drawing.Size(168, 54);
+            this.btnActualPrice.Size = new System.Drawing.Size(126, 44);
             this.btnActualPrice.TabIndex = 6;
             this.btnActualPrice.Text = "Актуализировать сайт";
             this.btnActualPrice.UseVisualStyleBackColor = true;
             this.btnActualPrice.Click += new System.EventHandler(this.btnActualPrice_Click);
             // 
+            // btnPrice
+            // 
+            this.btnPrice.Location = new System.Drawing.Point(543, 82);
+            this.btnPrice.Name = "btnPrice";
+            this.btnPrice.Size = new System.Drawing.Size(126, 29);
+            this.btnPrice.TabIndex = 7;
+            this.btnPrice.Text = "Обработать прайс";
+            this.btnPrice.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(919, 429);
+            this.ClientSize = new System.Drawing.Size(689, 349);
+            this.Controls.Add(this.btnPrice);
             this.Controls.Add(this.btnActualPrice);
             this.Controls.Add(this.btnSaveTemplate);
             this.Controls.Add(this.tbKeywords);
@@ -106,6 +124,7 @@
             this.Controls.Add(this.tbTitle);
             this.Controls.Add(this.rtbFullText);
             this.Controls.Add(this.rtbMiniText);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -122,6 +141,7 @@
         private System.Windows.Forms.TextBox tbKeywords;
         private System.Windows.Forms.Button btnSaveTemplate;
         private System.Windows.Forms.Button btnActualPrice;
+        private System.Windows.Forms.Button btnPrice;
     }
 }
 
