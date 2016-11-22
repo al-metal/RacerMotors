d0c34d11ac1802c31eb4bc2082c67309e156186a
al-metal <al-metal@bk.ru>
2016-11-22 08:47:40 +0400

    Обновленный файл

--- RacerMotors/nethouse.cs
+++ RacerMotors/nethouse.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -316,51 +317,7 @@ namespace Bike18
             newProduct.Add("Удалить *");                                    //удалить
             WriteFileInCSV(newProduct, nameFile);
         }
-
-        public string DownloadImages(CookieContainer cookie, string artProd)
-        {
-            string epoch = (DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalMilliseconds.ToString().Replace(",", "");
-            HttpWebRequest req = (HttpWebRequest)HttpWebRequest.Create("http://bike18.nethouse.ru/putimg?fileapi" + epoch);
-            req.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
-            req.UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:44.0) Gecko/20100101 Firefox/44.0";
-            req.Method = "POST";
-            req.ContentType = "multipart/form-data; boundary=---------------------------12709277337355";
-            req.CookieContainer = cookie;
-            req.Headers.Add("X-Requested-With", "XMLHttpRequest");
-            byte[] pic = File.ReadAllBytes("Pic\\" + artProd + ".jpg");
-            byte[] end = Encoding.ASCII.GetBytes("\r\n-----------------------------12709277337355\r\nContent-Disposition: form-data; name=\"_file\"\r\n\r\n" + artProd + ".jpg\r\n-----------------------------12709277337355--\r\n");
-            byte[] ms1 = Encoding.ASCII.GetBytes("-----------------------------12709277337355\r\nContent-Disposition: form-data; name=\"file\"; filename=\"" + artProd + ".jpg\"\r\nContent-Type: image/jpeg\r\n\r\n");
-            req.ContentLength = ms1.Length + pic.Length + end.Length;
-            Stream stre1 = req.GetRequestStream();
-            stre1.Write(ms1, 0, ms1.Length);
-            stre1.Write(pic, 0, pic.Length);
-            stre1.Write(end, 0, end.Length);
-            stre1.Close();
-            HttpWebResponse resimg = (HttpWebResponse)req.GetResponse();
-            StreamReader ressrImg = new StreamReader(resimg.GetResponseStream());
-            string otvimg = ressrImg.ReadToEnd();
-            return otvimg;
-        }
-
-        public string SaveImages(CookieContainer cookie, string urlSaveImg, int prodId, double widthImg, double heigthImg)
-        {
-            HttpWebRequest req = (HttpWebRequest)HttpWebRequest.Create("http://bike18.nethouse.ru/api/catalog/save-image");
-            req.Accept = "application/json, text/plain, */*";
-            req.UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:44.0) Gecko/20100101 Firefox/44.0";
-            req.Method = "POST";
-            req.ContentType = "application/x-www-form-urlencoded";
-            req.CookieContainer = cookie;
-            byte[] saveImg = Encoding.ASCII.GetBytes("url=" + urlSaveImg + "&id=0&type=4&objectId=" + prodId + "&imgCrop[x]=0&imgCrop[y]=0&imgCrop[width]=" + widthImg + "&imgCrop[height]=" + heigthImg + "&imageId=0&iObjectId=" + prodId + "&iImageType=4&replacePhoto=0");
-            req.ContentLength = saveImg.Length;
-            Stream srSave = req.GetRequestStream();
-            srSave.Write(saveImg, 0, saveImg.Length);
-            srSave.Close();
-            HttpWebResponse resSave = (HttpWebResponse)req.GetResponse();
-            StreamReader ressrSave = new StreamReader(resSave.GetResponseStream());
-            string otvSave = ressrSave.ReadToEnd();
-            return otvSave;
-        }
-
+        
         public string alsoBuyTovars(List<string> tovarList)
         {
             string name = tovarList[4].ToString();
@@ -378,6 +335,81 @@ namespace Bike18
             }
             return alsoBuy;
         }
+
+        #region Загрузка картинки товара на сайта Bike18.ru
+        public void UploadImage(CookieContainer cookieBike18, string urlProduct)
+        {
+            string otv = null;
+            urlProduct = urlProduct.Replace("http://bike18.ru", "http://bike18.nethouse.ru");
+
+            otv = webRequest.PostRequest(cookieBike18, urlProduct);
+            string artProd = new Regex("(?<=Артикул:)[\\w\\W]*?(?=</title><)").Match(otv).ToString().Trim();
+
+            MatchCollection prId = new Regex("(?<=data-id=\").*?(?=\")").Matches(otv);
+            string productId = prId[0].ToString();
+
+            Image newImg = Image.FromFile("Pic\\" + artProd + ".jpg");
+            double widthImg = newImg.Width;
+            double heigthImg = newImg.Height;
+
+            if (widthImg > heigthImg)
+            {
+                double dblx = widthImg * 0.9;
+                if (dblx < heigthImg)
+                    heigthImg = heigthImg * 0.9;
+                else
+                    widthImg = widthImg * 0.9;
+            }
+            else
+            {
+                double dblx = heigthImg * 0.9;
+                if (dblx < widthImg)
+                    widthImg = widthImg * 0.9;
+                else
+                    heigthImg = heigthImg * 0.9;
+            }
+
+            string epoch = (DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalMilliseconds.ToString().Replace(",", "");
+            HttpWebRequest req = (HttpWebRequest)HttpWebRequest.Create("https://bike18.nethouse.ru/putimg?fileapi" + epoch);
+            req.Accept = "*/*";
+            req.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36";
+            req.Method = "POST";
+            req.ContentType = "multipart/form-data; boundary=----WebKitFormBoundaryDxXeyjY3R0nRHgrP";
+            req.CookieContainer = cookieBike18;
+            req.Headers.Add("X-Requested-With", "XMLHttpRequest");
+            byte[] pic = File.ReadAllBytes("Pic\\" + artProd + ".jpg");
+            byte[] end = Encoding.ASCII.GetBytes("\r\n------WebKitFormBoundaryDxXeyjY3R0nRHgrP\r\nContent-Disposition: form-data; name=\"_file\"\r\n\r\n" + artProd + ".jpg\r\n------WebKitFormBoundaryDxXeyjY3R0nRHgrP--\r\n");
+            byte[] ms1 = Encoding.ASCII.GetBytes("------WebKitFormBoundaryDxXeyjY3R0nRHgrP\r\nContent-Disposition: form-data; name=\"file\"; filename=\"4680329013422.jpg\"\r\nContent-Type: image/jpeg\r\n\r\n");
+            req.ContentLength = ms1.Length + pic.Length + end.Length;
+            Stream stre1 = req.GetRequestStream();
+            stre1.Write(ms1, 0, ms1.Length);
+            stre1.Write(pic, 0, pic.Length);
+            stre1.Write(end, 0, end.Length);
+            stre1.Close();
+            HttpWebResponse resimg = (HttpWebResponse)req.GetResponse();
+            StreamReader ressrImg = new StreamReader(resimg.GetResponseStream());
+            string otvimg = ressrImg.ReadToEnd();
+            resimg.Close();
+            string urlSaveImg = new Regex("(?<=url\":\").*?(?=\")").Match(otvimg).Value.Replace("\\/", "%2F");
+
+            req = (HttpWebRequest)HttpWebRequest.Create("https://bike18.nethouse.ru/api/catalog/save-image");
+            req.Accept = "application/json, text/plain, */*";
+            req.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36";
+            req.Method = "POST";
+            req.ContentType = "application/x-www-form-urlencoded";
+            req.CookieContainer = cookieBike18;
+            byte[] saveImg = Encoding.ASCII.GetBytes("url=" + urlSaveImg + "&id=0&type=4&objectId=" + productId + "&imgCrop[x]=0&imgCrop[y]=0&imgCrop[width]=" + widthImg + "&imgCrop[height]=" + heigthImg + "&imageId=0&iObjectId=" + productId + "&iImageType=4&replacePhoto=0");
+            req.ContentLength = saveImg.Length;
+            Stream srSave = req.GetRequestStream();
+            srSave.Write(saveImg, 0, saveImg.Length);
+            srSave.Close();
+            HttpWebResponse resSave = (HttpWebResponse)req.GetResponse();
+            StreamReader ressrSave = new StreamReader(resSave.GetResponseStream());
+            ressrImg.Close();
+
+            string otvSave = ressrSave.ReadToEnd();
+        }
+        #endregion
 
         #region UploadCSVInNethouse
 
